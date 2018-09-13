2c8521b79f772c38b777374e8ef812c20c17bd7e
vincent <vincentr@live.nl>
2018-09-13 13:26:39 +0200

    49 -> 39 typo

--- runAll.sh
+++ runAll.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 clear
-javac -cp contest.jar player49.java
-jar cmf MainClass.txt submission.jar player49.class
-java -jar testrun.jar -submission=player49 -evaluation=SphereEvaluation -seed=1
+javac -cp contest.jar player39.java
+jar cmf MainClass.txt submission.jar player39.class
+java -jar testrun.jar -submission=player39 -evaluation=SphereEvaluation -seed=1
 
