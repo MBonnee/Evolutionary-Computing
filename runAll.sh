8090ebf52a31d73f2b1cfa67e86c03cffd125663
vincent <vincentr@live.nl>
2018-10-20 10:57:17 +0200

    new plotting benchmark vs POP_SIZE

--- runAll.sh
+++ runAll.sh
@@ -4,15 +4,22 @@ javac -cp contest.jar $(find . -name "*.java")
 jar cmf MainClass.txt submission.jar $(find . -name "*.class")
 
 # GRID SEARCH
+MIG_POP_COUNTER=0
+while [  $MIG_POP_COUNTER -lt 31 ]; do
+
 COUNTER=0
 while [  $COUNTER -lt 10 ]; do
-java -DNumIslands=20 -DPopSize=20 -DMIG_POP=3 -DBenchmark=false -jar testrun.jar -submission=player39 -evaluation=SchaffersEvaluation -seed=1 > txt/x_ladder_score_$COUNTER.txt
+java -DNumIslands=20 -DPopSize=20 -DMIG_POP=$MIG_POP_COUNTER -DBenchmark=false -jar testrun.jar -submission=player39 -evaluation=SchaffersEvaluation -seed=1 > txt/x_${MIG_POP_COUNTER}_ladder_score_${COUNTER}.txt
 let COUNTER=COUNTER+1
 done
 
 COUNTER=0
 while [  $COUNTER -lt 10 ]; do
-java -DNumIslands=20 -DPopSize=20 -DMIG_POP=3 -DBenchmark=true -jar testrun.jar -submission=player39 -evaluation=SchaffersEvaluation -seed=1 > txt/x_benchmark_score_$COUNTER.txt
+java -DNumIslands=20 -DPopSize=20 -DMIG_POP=$MIG_POP_COUNTER -DBenchmark=true -jar testrun.jar -submission=player39 -evaluation=SchaffersEvaluation -seed=1 > txt/x_${MIG_POP_COUNTER}_benchmark_score_${COUNTER}.txt
 let COUNTER=COUNTER+1
+done
+
+let MIG_POP_COUNTER=MIG_POP_COUNTER+3
+
 done
 
